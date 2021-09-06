1418734122cd204723514b93933aacd733c1d4d1
elliemay481 <elliemay481@gmail.com>
2021-09-06 20:48:18 +0200

    Continue isolating regions

--- Prepackage/2Dtoy.py
+++ Prepackage/2Dtoy.py
@@ -60,17 +60,17 @@ kern = kernels.SE()
 def history_match(parameter_bounds, sigma_cov, var_exp, beta, ndim, N_training_pts, N_test_pts, waves=1):
     
     # generate initial well spaced inputs for train and test sets
-    input_train, input_test = data.prepare_data(ndim, Nsamples, Ntraining, parameter_bounds)
+    input_train_all, input_test_all = data.prepare_data(ndim, Nsamples, Ntraining, parameter_bounds)
 
     # evaluate true model over training inputs
-    output_train = np.zeros(Ntraining)
+    output_train_all = np.zeros(Ntraining)
     for i in range(Ntraining):
-        output_train[i] = true_model(input_train[i,0], input_train[i,1])
+        output_train_all[i] = true_model(input_train_all[i,0], input_train_all[i,1])
 
     # artificially add noise to observations
-    output_train += np.random.normal(0,var_exp)
+    output_train_all += np.random.normal(0,var_exp)
 
-    N_test_pts = len(input_test)
+    N_test_pts = len(input_test_all)
     
     # plot settings
     fig, axes = plt.subplots(waves, 3, figsize=(16, 6*waves))
@@ -88,110 +88,143 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, ndim, N_training_p
 
         print('Current wave: ' + str(k+1))
 
-        if N_regions == 1:
-            input_train = [input_train_n]
-            input_test = [input_test_n]
-            output_train
+        input_train_list = []
+        input_test_list = []
+        output_train_list = []
 
         # iterate over nonimplausible regions
-        for n in N_regions:
+        for n in range(N_regions):
 
+            print(n)
+
+            if N_regions == 1:
+                input_train = input_train_all
+                input_test = input_test_all
+                output_train = output_train_all
+            else:
+                input_train = input_train_all[n]
+                input_test = input_test_all[n]
+                output_train = output_train_all[n]
+
+            #print(input_train)
 
         
-        # build emulator over nonimplausible region
-        GP = emulator.Gaussian_Process(input_train, input_test, output_train, sigma_cov, beta, kern)
-        
-         # optimise hyperparameters of emulator
-        GP.optimise()
-        # fit emulator using training points
-        mu, cov, sd = GP.emulate()
-        
-        implaus = np.zeros(N_test_pts)
-        for i in range(N_test_pts):
-            implaus[i] = historymatch.implausibility(mu[i], z, sd[i], 0, var_exp)
-        
-        # plot implausibilities
-        ax1 = ax_list[3*k]
-        ax2 = ax_list[3*k + 1]
-        ax3 = ax_list[3*k + 2]
-        im =  ax1.tricontourf(input_test[:,0],input_test[:,1],implaus, levels=20, cmap='viridis_r')
-        ax1.set_facecolor((68/255,1/255,84/255))
-        ax1.set_xlim([xmin_0, xmax_0])
-        ax1.set_ylim([ymin_0, ymax_0])
-        cbar = fig.colorbar(im, ax=ax1)
-        cbar.set_label('Implausibility')
-        im.set_clim(0,10)
-        ax1.set_ylabel('y')
-        ax1.set_xlabel('x')
-        ax1.set_title('Wave ' + str(k+1) + ' Implausibility')
-        ax1.scatter(true_x, true_y, color='red', marker='x', label='Observed Data')
-        
-        # identify implausible region
-        input_imp = np.concatenate((input_test, implaus.reshape(-1,1)), axis=1)
-        nonimplausible = np.delete(input_imp, np.where(input_imp[:,2] > 3), axis=0)
-
-        # isolate implausible regions based on greatest y difference
-        implaus_regions = group(nonimplausible)
-        N_regions = len(implaus_regions)
-        
-        # identify nonimplausible region boundaries and plot
-        for i in range(N_regions):
-            group_i = group(nonimplausible)[i]
-
-            xmin_i = group_i[:,0].min()
-            xmax_i = group_i[:,0].max()
-            ymin_i = group_i[:,1].min()
-            ymax_i = group_i[:,1].max()
-            ax2.vlines(x=[xmin_i, xmax_i], ymin=ymin_i, ymax=ymax_i, linestyle = '--', color='pink')
-            ax2.hlines(y=[ymin_i, ymax_i], xmin=xmin_i, xmax=xmax_i, linestyle = '--', color='pink')
-
-        print(implaus_regions.shape)
-
-    
-        # find nonimplausible boundaries
-        x_bound = np.array([nonimplausible[:,0].min(), nonimplausible[:,0].max()]).reshape(1,-1)
-        y_bound = np.array([nonimplausible[:,1].min(), nonimplausible[:,1].max()]).reshape(1,-1)
-        
-        xmin = parameter_bounds[0,0]
-        xmax = parameter_bounds[0,1]
-        ymin = parameter_bounds[1,0]
-        ymax = parameter_bounds[1,1]
-
-        # redefine nonimplausible space & generate new training points in nonimplausible region
-        parameter_bounds = np.concatenate((x_bound, y_bound), axis=0)
-        input_train, input_test = data.prepare_data(ndim, Nsamples, Ntraining, parameter_bounds)
-
-        # evaluate model over new training data
-        output_train = np.zeros(N_training_pts)
-        for i in range(N_training_pts):
-            output_train[i] = true_model(input_train[i,0], input_train[i,1])
-        
-        # plot nonimplausible datapoints
-        ax2.scatter(nonimplausible[:,0], nonimplausible[:,1], s=2)
-        ax2.scatter(true_x, true_y, color='red', marker='x', label='Observed Data')
-        ax2.set_title('Remaining Non-Implausible Datapoints')
-        ax2.set_xlabel('x')
-        ax2.set_ylabel('y')
-        ax2.set_xlim([-3,3])
-        ax2.set_ylim([-3,3])
-        ax2.scatter(input_train[:,0], input_train[:,1], marker='x', color='black', label='Training Data')
-        #ax2.axhline(ymin, linestyle = '--', color='pink')
-        #ax2.axhline(ymax, linestyle = '--', color='pink')
-        #ax2.axvline(xmin, linestyle = '--', color='pink')
-        #ax2.axvline(xmax, linestyle = '--', color='pink')
-        ax2.legend(loc='lower left')
-        
+            # build emulator over nonimplausible region
+            GP = emulator.Gaussian_Process(input_train, input_test, output_train, sigma_cov, beta, kern)
             
-        n_grid = 20
-        hist, xedges, yedges = np.histogram2d(nonimplausible[:,1], nonimplausible[:,0], bins=n_grid, range=[[-2, 2], [-2, 2]])
+            # optimise hyperparameters of emulator
+            GP.optimise()
+            # fit emulator using training points
+            mu, cov, sd = GP.emulate()
+            
+            implaus = np.zeros(N_test_pts)
+            for i in range(N_test_pts):
+                implaus[i] = historymatch.implausibility(mu[i], z, sd[i], 0, var_exp)
+            
+            # plot implausibilities
+            ax1 = ax_list[3*k]
+            ax2 = ax_list[3*k + 1]
+            ax3 = ax_list[3*k + 2]
+            im =  ax1.tricontourf(input_test[:,0],input_test[:,1],implaus, levels=20, cmap='viridis_r')
+            ax1.set_facecolor((68/255,1/255,84/255))
+            ax1.set_xlim([xmin_0, xmax_0])
+            ax1.set_ylim([ymin_0, ymax_0])
+            cbar = fig.colorbar(im, ax=ax1)
+            cbar.set_label('Implausibility')
+            im.set_clim(0,10)
+            ax1.set_ylabel('y')
+            ax1.set_xlabel('x')
+            ax1.set_title('Wave ' + str(k+1) + ' Implausibility')
+            ax1.scatter(true_x, true_y, color='red', marker='x', label='Observed Data')
+            
+            # identify implausible region
+            input_imp = np.concatenate((input_test, implaus.reshape(-1,1)), axis=1)
+            nonimplausible = np.delete(input_imp, np.where(input_imp[:,2] > 3), axis=0)
 
-        im2 = ax3.contourf(np.linspace(-2,2,n_grid),np.linspace(-2,2,n_grid),hist/((100/20)**2),cmap='pink')
-        cbar = fig.colorbar(im2, ax=ax3)
-        im2.set_clim(0,1)
-        
-        #ax3.plot(x_test, implaus[:,0])
-        #ax3.set_ylabel('Implausibility')
-        #ax3.set_xlabel('x')
+            # isolate implausible regions based on greatest y difference
+            implaus_regions = group(nonimplausible)
+            N_regions = len(implaus_regions)
+            
+            
+            # identify nonimplausible region boundaries and plot
+            for i in range(N_regions):
+                group_i = group(nonimplausible)[i]
+
+                xmin_i = group_i[:,0].min()
+                xmax_i = group_i[:,0].max()
+                ymin_i = group_i[:,1].min()
+                ymax_i = group_i[:,1].max()
+                ax2.vlines(x=[xmin_i, xmax_i], ymin=ymin_i, ymax=ymax_i, linestyle = '--', color='pink')
+                ax2.hlines(y=[ymin_i, ymax_i], xmin=xmin_i, xmax=xmax_i, linestyle = '--', color='pink')
+
+                # redefine nonimplausible space & generate new training points
+                # find nonimplausible boundaries
+                x_bound = np.array([xmin_i, xmax_i]).reshape(1,-1)
+                y_bound = np.array([ymin_i, ymax_i]).reshape(1,-1)
+                parameter_bounds = np.concatenate((x_bound, y_bound), axis=0)
+                input_train_i, input_test_i = data.prepare_data(ndim, Nsamples, Ntraining, parameter_bounds)
+
+                input_train_list.append(input_train_i)
+                input_test_list.append(input_test_i)
+
+                # evaluate model over new training data
+                output_train_i = np.zeros(N_training_pts)
+                for i in range(N_training_pts):
+                    output_train_i[i] = true_model(input_train_i[i,0], input_train_i[i,1])
+
+                output_train_list.append(output_train_i)
+
+            input_train_all = input_train_list 
+            input_test_all = input_test_list
+            output_train_all = output_train_list
+
+            print(input_train_all[1])
+
+            
+            
+            #xmin = parameter_bounds[0,0]
+            #xmax = parameter_bounds[0,1]
+            #ymin = parameter_bounds[1,0]
+            #ymax = parameter_bounds[1,1]
+
+            # redefine nonimplausible space & generate new training points in nonimplausible region
+            #parameter_bounds = np.concatenate((x_bound, y_bound), axis=0)
+            #input_train, input_test = data.prepare_data(ndim, Nsamples, Ntraining, parameter_bounds)
+
+            # evaluate model over new training data
+            #output_train = np.zeros(N_training_pts)
+            #for i in range(N_training_pts):
+                #output_train[i] = true_model(input_train[i,0], input_train[i,1])
+
+
+            
+            '''
+            # plot nonimplausible datapoints
+            ax2.scatter(nonimplausible[:,0], nonimplausible[:,1], s=2)
+            ax2.scatter(true_x, true_y, color='red', marker='x', label='Observed Data')
+            ax2.set_title('Remaining Non-Implausible Datapoints')
+            ax2.set_xlabel('x')
+            ax2.set_ylabel('y')
+            ax2.set_xlim([-3,3])
+            ax2.set_ylim([-3,3])
+            ax2.scatter(input_train[:,0], input_train[:,1], marker='x', color='black', label='Training Data')
+            #ax2.axhline(ymin, linestyle = '--', color='pink')
+            #ax2.axhline(ymax, linestyle = '--', color='pink')
+            #ax2.axvline(xmin, linestyle = '--', color='pink')
+            #ax2.axvline(xmax, linestyle = '--', color='pink')
+            ax2.legend(loc='lower left')
+            
+                
+            n_grid = 20
+            hist, xedges, yedges = np.histogram2d(nonimplausible[:,1], nonimplausible[:,0], bins=n_grid, range=[[-2, 2], [-2, 2]])
+
+            im2 = ax3.contourf(np.linspace(-2,2,n_grid),np.linspace(-2,2,n_grid),hist/((100/20)**2),cmap='pink')
+            cbar = fig.colorbar(im2, ax=ax3)
+            im2.set_clim(0,1)
+            
+            #ax3.plot(x_test, implaus[:,0])
+            #ax3.set_ylabel('Implausibility')
+            #ax3.set_xlabel('x')'''
 
 history_match(input_bounds, sigma_cov, var_exp, beta, 2, Ntraining, Nsamples, 1)
 
