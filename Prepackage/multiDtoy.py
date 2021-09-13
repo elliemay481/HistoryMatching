7f64f0b4a20268c21bcd3b2fb0774ac978416b91
elliemay481 <elliemay481@gmail.com>
2021-09-13 18:49:17 +0200

    Update optical depth plots

--- Prepackage/multiDtoy.py
+++ Prepackage/multiDtoy.py
@@ -18,8 +18,8 @@ def model_eqn_1(x, y, a):
     #return a + y + x
 
 def model_eqn_2(x, y, a):
-    return np.sin(np.sqrt(x**2 + 2*y**2 + 3*a**2))
-    #return a - y - x
+    #return np.sin(np.sqrt(x**2 + 4*y**2 + 2*a**2))
+    return a**2 - y + x
 
 true_model = [model_eqn_1, model_eqn_2]
 
@@ -36,10 +36,10 @@ input_bounds = np.concatenate((x_bound, y_bound, a_bound), axis=0)
 
 # for testing: true datapoints
 true_x = -0.1
-true_y = 0.2
+true_y = 1.2
 true_a = 0.3
 true_parameters = [true_x, true_y, true_a]
-var_exp = 0.01        # observational uncertainty variance
+var_exp = 0.001        # observational uncertainty variance
 z_1 = model_eqn_1(true_x, true_y, true_a) + np.random.normal(0,var_exp) # observed datapoint
 z_2 = model_eqn_2(true_x, true_y, true_a) + np.random.normal(0,var_exp) # observed datapoint
 
@@ -65,6 +65,8 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, Ntraining, Nsample
     parameter_bounds_initial = parameter_bounds
     N_regions = 1
 
+    Nsamples_0 = Nsamples
+    Ntraining_0 = Ntraining
     for k in range(waves):
 
         print('Current wave: ' + str(k+1))
@@ -75,8 +77,11 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, Ntraining, Nsample
 
         # plot settings
         gs00 = gridspec.GridSpecFromSubplotSpec(3, 3, subplot_spec=gs0[k], wspace=0.1, hspace=0.1)
-        ax2 = plt.axes(projection='3d')
-        #ax2 = plt.axes()
+        
+        p1axes = np.empty((3,3), dtype=object)
+        for i in range(3):
+                for j in range(3):
+                    p1axes[i,j] = fig1.add_subplot(gs00[i, j])
 
         # iterate over nonimplausible regions
         for n in range(N_regions):
@@ -98,15 +103,17 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, Ntraining, Nsample
 
 
                 if k == 0:
+                    ax2 = plt.axes(projection='3d')
                     # evaluate true model over training inputs
                     output_train = np.zeros(Ntraining)
                     true_model_vec = np.vectorize(true_model[output])
                     output_train = true_model_vec(*input_train.T)
                     # artificially add noise to observations
-                    #output_train += np.random.normal(0,var_exp)
+                    output_train += np.random.normal(0,var_exp)
                     
                 else:
                     output_train = output_train_all[n][output]
+
                 # build emulator over nonimplausible region
                 GP = emulator.Gaussian_Process(input_train, input_test, output_train, sigma_cov, beta, kern)
                 # optimise hyperparameters of emulator
@@ -114,62 +121,56 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, Ntraining, Nsample
                 # fit emulator using training points
                 mu, cov, sd = GP.emulate()
 
-
-                #print(zlist[output])
                 # evaluate implausibility over parameter volume
-                
                 for i in range(len(input_test)):
                     implaus_all[i, output] = historymatch.implausibility(mu[i], zlist[output], sd[i], 0, var_exp)
-
-                ax2.scatter(input_test[:,0],input_test[:,1], input_test[:,2], s=2, color = 'cornflowerblue', label='Implausible Points' if n == 0 else "")
-                #ax2.scatter(input_train[:,0], input_train[:,1], marker='x', color='black', label='Training Data' if n == 0 else "")
-            
+                    
+            # choose maximum implausibility
             max_I = np.argmax(implaus_all, axis=1)
             implaus = np.choose(max_I, implaus_all.T)
-            #print(implaus)
+
             # identify implausible region
             input_imp = np.concatenate((input_test, implaus.reshape(-1,1)), axis=1)
             # if region empty, skip
             nonimplausible = np.delete(input_imp, np.where(input_imp[:,-1] > 3), axis=0)
             if nonimplausible.size == 0:
+                print('empty')
                 continue
-            
 
             # plot implausibilities and optical depth
             variable_names = ['x', 'y', 'z']
             for i in range(3):
                 for j in range(3):
-                    ax1 = fig1.add_subplot(gs00[i, j])
+                    ax1 = p1axes[i,j]
                     variables = [i,j]
                     if i == j:
-                        plot.optical_depth_1D(nonimplausible, 20, ax1, fig1, i, variable_names[i], parameter_bounds_initial, input_test_0)
+                        plot.optical_depth_1D(input_imp, 20, ax1, fig1, i, variable_names[i], parameter_bounds_initial, input_test_0)
                     elif i > j:
-                        
                         plot.implausibility(input_imp, parameter_bounds_initial, ax1, fig1, k, n, variables, [variable_names[i], variable_names[j]])
-                        ax1.scatter(true_parameters[j], true_parameters[i], color='red', marker='x', label='Observed Data' if n == 0 else "")
+                        #ax1.scatter(true_parameters[j], true_parameters[i], color='red', marker='x', label='Observed Data' if n == 0 else "")
                     else:
-                        plot.optical_depth_2D(nonimplausible, 20, ax1, fig1, variables, [variable_names[i], variable_names[j]])
-                        ax1.scatter(true_parameters[j], true_parameters[i], color='red', marker='x', label='Observed Data' if n == 0 else "")
-
-            # plot nonimplausible datapoints
-            ax2.scatter(nonimplausible[:,0], nonimplausible[:,1], nonimplausible[:,2], color='red', s=3, label='Nonimplausible Points' if n == 0 else "")
-            #ax2.scatter(true_x, true_y, color='red', marker='x', label='Observed Data' if n == 0 else "")
-            ax2.set_title('Remaining Non-Implausible Datapoints')
-            ax2.set_xlabel('x')
-            ax2.set_ylabel('y')
-            ax2.set_xlim([parameter_bounds_initial[0,0],parameter_bounds_initial[0,1]])
-            ax2.set_ylim([parameter_bounds_initial[1,0],parameter_bounds_initial[1,1]])
-            ax2.legend(loc='lower left')
+                        plot.optical_depth_2D(input_imp, parameter_bounds_initial, input_test_0, ax1, fig1, k, n, variables, [variable_names[i], variable_names[j]])
 
             # isolate implausible regions based on greatest y difference
             implaus_regions = historymatch.find_clusters_3D(nonimplausible, input_test, ndim, parameter_bounds, n_grid=5)
-            
+            implaus_volumes = []
+            implaus_N_pts = []
             # identify nonimplausible region boundaries and plot
             for i in range(len(implaus_regions)):
 
-                #ax2.scatter(implaus_regions[i][:,0], implaus_regions[i][:,1], implaus_regions[i][:,2])
-                
                 group_i = implaus_regions[i]
+
+                # plot first wave regions to visually check results
+                if k == 0:
+                    
+                    ax2.set_title('Wave 1 Nonimplausible Regions')
+                    ax2.scatter(implaus_regions[i][:,0], implaus_regions[i][:,1], implaus_regions[i][:,2])
+                    ax2.set_xlabel('x')
+                    ax2.set_ylabel('y')
+                    ax2.set_zlabel('z')
+                    ax2.set_xlim([parameter_bounds_initial[0,0],parameter_bounds_initial[0,1]])
+                    ax2.set_ylim([parameter_bounds_initial[1,0],parameter_bounds_initial[1,1]])
+                
 
                 # find nonimplausible boundaries
                 parameter_bounds = np.empty((0,2))
@@ -189,29 +190,27 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, Ntraining, Nsample
                         volume = volume * (max_i - min_i)
 
                 # rescale number of points to sample in parameter space
-                ratio = volume/initial_volume
-                Ntraining = int(np.ceil(ratio*25)) + 3
-                #Nsamples = int(np.ceil(ratio*1000)) + 100
+                implaus_volumes.append(volume)
+                
+            total_volume = sum(implaus_volumes)
 
-                #ax2.vlines(x=[group_i[:,0].min(), group_i[:,0].max()], ymin=group_i[:,1].min(), ymax=group_i[:,1].max(), linestyle = '--', linewidth=2, color='red')
-                #ax2.hlines(y=[group_i[:,1].min(), group_i[:,1].max()], xmin=group_i[:,0].min(), xmax=group_i[:,0].max(), linestyle = '--', linewidth=2, color='red')
+            for i in range(len(implaus_regions)):
+
+                Ntraining = int(np.ceil((implaus_volumes[i]/total_volume)*Ntraining))
+                Nsamples = int(np.ceil((implaus_volumes[i]/total_volume)*Nsamples))
 
                 # redefine nonimplausible space & generate new training points
-                # generate initial well spaced inputs for train and test sets
                 input_train_i, input_test_i = data.prepare_data(ndim, Nsamples, Ntraining, parameter_bounds)
 
+                # evaluate true model over training inputs
                 output_train_region = []
-
                 for output in range(n_outputs):
-                    
-                    # evaluate true model over training inputs
                     output_train_i = np.zeros(len(input_train_i))
                     true_model_vec = np.vectorize(true_model[output])
                     output_train_i = true_model_vec(*input_train_i.T)
                     # artificially add noise to observations
-                    #output_train_i += np.random.normal(0,var_exp)
+                    output_train_i += np.random.normal(0,var_exp)
                     output_train_region.append(np.array(output_train_i))
-
 
                 input_train_list.append(input_train_i)
                 input_test_list.append(input_test_i)
@@ -228,7 +227,7 @@ def history_match(parameter_bounds, sigma_cov, var_exp, beta, Ntraining, Nsample
     fig2.savefig('test_plots_3D.png')
         
 
-history_match(input_bounds, sigma_cov, var_exp, beta, Ntraining, Nsamples, zlist=[z_1, z_2], ndim=3, n_outputs=2, waves=3)
+history_match(input_bounds, sigma_cov, var_exp, beta, Ntraining, Nsamples, zlist=[z_1, z_2], ndim=3, n_outputs=2, waves=4)
 
 
 plt.show()
--- Prepackage/plot.py
+++ Prepackage/plot.py
@@ -44,29 +44,31 @@ def implausibility(input_imp, parameter_bounds_initial, ax, fig, wave, region, v
             y_indices = np.where(np.logical_and(input_imp[:,variables[0]]>=ybin0, input_imp[:,variables[0]]<ybin1))
             x_indices = np.where(np.logical_and(input_imp[:,variables[1]]>=xbin0, input_imp[:,variables[1]]<xbin1))
             bin_indices = np.intersect1d(x_indices, y_indices)
+            # find minimised implausibility over remaining dimensions
             if bin_indices.shape[0] != 0:
                 implausibilities[i,j] = input_imp[bin_indices][:,-1].min()
             else:
                 implausibilities[i,j] = np.NaN
-    
-    bounds=[0,0.5,1,1.5,2,2.5,3,3.5]
+
+    if np.nanmax(implausibilities) > 3.5:
+        bounds=[0,0.5,1,1.5,2,2.5,3,3.5]
+    else:
+        bounds = np.linspace(np.nanmin(implausibilities), np.nanmax(implausibilities), 8)
     cmap = cm.get_cmap('viridis_r', len(bounds))
 
     im = ax.contourf(xvals, yvals, implausibilities, levels=bounds, colors=[cmap(0), cmap(0.1), cmap(0.15), cmap(0.3), cmap(0.45), cmap(0.6), cmap(0.85), cmap(1)])
     #im = ax.contourf(xvals, yvals, implausibilities, cmap='viridis_r')
 
 
-    #im =  ax.tricontourf(input_test[:,variables[1]],input_test[:,variables[0]],implaus, levels=[0, 0.5, 1, 2.5, 2.6, 2.7, 3], cmap='viridis_r')
-
     ax.set_facecolor((68/255,1/255,84/255))
     ax.set_xlim([parameter_bounds_initial[0,0], parameter_bounds_initial[0,1]])
     ax.set_ylim([parameter_bounds_initial[1,0], parameter_bounds_initial[1,1]])
-    cbar = fig.colorbar(im, ax=ax)
-    cbar.set_label('Implausibility')
-    im.set_clim(0,10)
+    #cbar = fig.colorbar(im, ax=ax)
+    #cbar.set_label('Implausibility')
+    #im.set_clim(0,10)
     ax.set_ylabel(variable_names[0])
     ax.set_xlabel(variable_names[1])
-    cbar.remove()
+    #cbar.remove()
     #ax.set_title('Wave ' + str(wave+1) + ' Implausibility')
     if variables[0] != input_imp.shape[1] - 2:
         ax.axes.xaxis.set_visible(False)
@@ -97,21 +99,9 @@ def nonimplausible_pts(input_train, nonimplausible, ax):
     ax.axvline(xmax, linestyle = '--', color='pink')
     ax.legend(loc='lower left')
 
-def optical_depth_2D(nonimplausible, ngrid, ax, fig, variables, variable_names):
-    hist, xedges, yedges = np.histogram2d(nonimplausible[:,variables[0]], nonimplausible[:,variables[1]], bins=ngrid)
-    im = ax.contourf(np.linspace(nonimplausible[:,variables[0]].min(),nonimplausible[:,variables[0]].max(),ngrid),
-                        np.linspace(nonimplausible[:,variables[1]].min(),nonimplausible[:,variables[1]].max(),ngrid),hist/((100/20)**2),cmap='magma')
-    #cbar = fig.colorbar(im, ax=ax)
-    #im.set_clim(0,0.5)
-    ax.set_ylabel(variable_names[0])
-    ax.set_xlabel(variable_names[1])
-    ax.axes.xaxis.set_visible(False)
-    ax.axes.yaxis.set_visible(False)
-    ax.set_xlim([-2,2])
-    ax.set_ylim([-2,2])
-    ax.set_facecolor('black')
 
-def optical_depth_1D(nonimplausible, bins, ax, fig, variable, variable_name, parameter_bounds_initial, input_test_0):
+
+def optical_depth_1D(input_imp, bins, ax, fig, variable, variable_name, parameter_bounds_initial, input_test_0):
     #print(nonimplausible[:,variable])
     #ax.hist(nonimplausible[:,variable], bins=ngrid)
 
@@ -119,32 +109,95 @@ def optical_depth_1D(nonimplausible, bins, ax, fig, variable, variable_name, par
     bound0 = parameter_bounds_initial[variable, 0]
     bound1 = parameter_bounds_initial[variable, 1]
     bin_size = (bound1 - bound0)/bins
-    #print(bin_size)
+
 
     bin_list = []
     counts = []
     initial_density = 0
 
     for i in range(bins):
+        # define bin
         bin0 = bound0 + i*bin_size
         bin1 = bin0 + bin_size
-        #print('0: ' + str(bin0))
-        #print(bin1)
         bin_list.append(bin0 + (bin1-bin0)/2)
         # find values within bin
-        indices = np.where(np.logical_and(nonimplausible[:,variable]>=bin0, nonimplausible[:,variable]<bin1))
-        # find original density in this bin
-        indices_initial = len(np.where(np.logical_and(input_test_0[:,variable]>=bin0, input_test_0[:,variable]<bin1))[0])
-        bin_count = np.count_nonzero(nonimplausible[indices,-1] < 3, axis=1)
-        counts.append(bin_count[0]/indices_initial)
+        indices = np.where(np.logical_and(input_imp[:,variable]>=bin0, input_imp[:,variable]<bin1))
+        # find initial density in this bin
+        initial_space = len(indices)
+        # for each bin, count nonimplausible values over remaining parameter volume
+        bin_count = np.count_nonzero(input_imp[indices,-1] < 3, axis=1)
+        counts.append(bin_count[0]/initial_space)
 
     ax.bar(bin_list, counts)
 
-    #im = ax.contourf(np.linspace(-2,2,ngrid),np.linspace(-2,2,ngrid),hist/((100/20)**2),cmap='pink')
-    #cbar = fig.colorbar(im, ax=ax)
-    #im.set_clim(0,0.5)
-    if variable != nonimplausible.shape[1] - 2:
+
+    if variable != input_imp.shape[1] - 2:
         ax.axes.xaxis.set_visible(False)
     if variable != 0:
         ax.axes.yaxis.set_visible(False)
     ax.set_xlim([-2,2])
+
+def optical_depth_2D(input_imp, parameter_bounds_initial, input_test_0, ax, fig, wave, region, variables, variable_names, bins=20):
+    # split axis into bins
+    ybound0 = parameter_bounds_initial[variables[0], 0]
+    ybound1 = parameter_bounds_initial[variables[0], 1]
+    xbound0 = parameter_bounds_initial[variables[1], 0]
+    xbound1 = parameter_bounds_initial[variables[1], 1]
+    bin_width = (xbound1 - xbound0)/bins
+    bin_height = (ybound1 - ybound0)/bins
+
+    densities = np.zeros((bins, bins))
+    xvals = np.linspace(xbound0,xbound1,bins)
+    yvals = np.linspace(ybound0,ybound1,bins)
+
+    for i in range(bins): # across y
+        ybin0 = ybound0 + i*bin_height
+        ybin1 = ybin0 + bin_height
+        # add mid of cell to list for plotting
+        for j in range(bins):  # across x
+            xbin0 = xbound0 + j*bin_width
+            xbin1 = xbin0 + bin_height
+
+            # find points within bin
+            y_indices = np.where(np.logical_and(input_imp[:,variables[0]]>=ybin0, input_imp[:,variables[0]]<ybin1))
+            x_indices = np.where(np.logical_and(input_imp[:,variables[1]]>=xbin0, input_imp[:,variables[1]]<xbin1))
+            bin_indices = np.intersect1d(x_indices, y_indices)
+            # count nonimplausible points over remaining dimensions
+            n_pts = np.count_nonzero(input_imp[bin_indices][:,-1] < 3)
+            # find initial number of points 
+            y_indices_0 = np.where(np.logical_and(input_imp[:,variables[0]]>=ybin0, input_imp[:,variables[0]]<ybin1))
+            x_indices_0 = np.where(np.logical_and(input_imp[:,variables[1]]>=xbin0, input_imp[:,variables[1]]<xbin1))
+            bin_indices_0 = np.intersect1d(x_indices_0, y_indices_0)
+
+            if len(bin_indices_0) != 0:
+                densities[i,j] = n_pts/len(bin_indices_0)
+            else:
+                densities[i,j] = 0
+            
+
+    
+    #bounds=np.linspace(np.amin(densities), np.amax(densities), 8)
+    bounds = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.8]
+    cmap = cm.get_cmap('magma', len(bounds))
+    #print(np.amin(densities))
+    #print(np.amax(densities))
+    im = ax.contourf(xvals, yvals, densities, levels=bounds, colors=[cmap(0), cmap(0.1), cmap(0.15), cmap(0.3), cmap(0.45), cmap(0.6), cmap(0.85), cmap(1)])
+    #im = ax.contourf(xvals, yvals, densities, cmap='viridis_r')
+
+
+    #im =  ax.tricontourf(input_test[:,variables[1]],input_test[:,variables[0]],implaus, levels=[0, 0.5, 1, 2.5, 2.6, 2.7, 3], cmap='viridis_r')
+
+    ax.set_facecolor((68/255,1/255,84/255))
+    ax.set_xlim([parameter_bounds_initial[0,0], parameter_bounds_initial[0,1]])
+    ax.set_ylim([parameter_bounds_initial[1,0], parameter_bounds_initial[1,1]])
+    #cbar = fig.colorbar(im, ax=ax)
+    #cbar.set_label('Implausibility')
+    #im.set_clim(0,10)
+    ax.set_ylabel(variable_names[0])
+    ax.set_xlabel(variable_names[1])
+    #cbar.remove()
+    #ax.set_title('Wave ' + str(wave+1) + ' Implausibility')
+    if variables[0] != input_imp.shape[1] - 2:
+        ax.axes.xaxis.set_visible(False)
+    if variables[1] != 0:
+        ax.axes.yaxis.set_visible(False)
